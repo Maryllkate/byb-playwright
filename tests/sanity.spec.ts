4a1460e1a8f8d8cc67d9320addad47735485a7bd
Maryll Driz <marylldriz@Marylls-MacBook-Pro.local>
2024-12-07 21:38:57 +0800

    adding typescript linting

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('IDS login page', async ({ page }) => {
+test('IDS access page', async ({ page }) => {
   await page.goto('https://qa-directsale.beforeyoubuy.com.au/login');
-
 });
